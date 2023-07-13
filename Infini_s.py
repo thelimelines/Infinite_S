7820f94c6918a0a472b8d1abc83af182f2e91ad2
thelimelines <brodierolph1@gmail.com>
2023-07-13 17:32:31 +0100

    ROTATION

--- Infini_s.py
+++ Infini_s.py
@@ -2,24 +2,53 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 
-# Define the coordinates for the Super S as separate line segments
-segments = [
-    np.array([[-1, 0.5], [-1, 1.5]]), 
-    np.array([[0, 0.5], [0, 1.5]]), 
-    np.array([[1, 0.5], [1, 1.5]]), 
-    np.array([[1, -0.5], [1, -1.5]]), 
-    np.array([[0, -0.5], [0, -1.5]]), 
-    np.array([[-1, -0.5], [-1, -1.5]]),
-    np.array([[0,-0.5],[-1,0.5]]),
-    np.array([[1,-0.5],[0,0.5]]),
-    np.array([[-1, 1.5], [0, 2.5]]), 
-    np.array([[0, 2.5], [1, 1.5]]), 
-    np.array([[1,-1.5], [0, -2.5]]),
-    np.array([[0, -2.5], [-1, -1.5]]),
-    np.array([[0,-0.5],[-1,0.5]]),
-    np.array([[-1,-0.5],[-0.5,0]]),
-    np.array([[0.5,0],[1,0.5]])
-]
+def create_stussy_s_segments(top, bottom):
+    # Original 'Stussy S' segments centered at (0,0)
+    norm_segments = [
+        np.array([[-0.2, 0.1], [-0.2, 0.3]]), 
+        np.array([[0, 0.1], [0, 0.3]]), 
+        np.array([[0.2, 0.1], [0.2, 0.3]]), 
+        np.array([[0.2, -0.1], [0.2, -0.3]]), 
+        np.array([[0, -0.1], [0, -0.3]]), 
+        np.array([[-0.2, -0.1], [-0.2, -0.3]]),
+        np.array([[0,-0.1],[-0.2,0.1]]),
+        np.array([[0.2,-0.1],[0,0.1]]),
+        np.array([[-0.2, 0.3], [0, 0.5]]), 
+        np.array([[0, 0.5], [0.2, 0.3]]), 
+        np.array([[0.2,-0.3], [0, -0.5]]),
+        np.array([[0, -0.5], [-0.2, -0.3]]),
+        np.array([[-0.2,-0.1],[-0.1,0]]),
+        np.array([[0.1,0],[0.2,0.1]])
+    ]
+    
+    # Calculate the target size and angle
+    size = np.linalg.norm(top - bottom)
+    angle = np.arctan2(top[1] - bottom[1], top[0] - bottom[0])
+
+    # Create the rotation matrix
+    rot_matrix = np.array([
+        [np.cos(angle), -np.sin(angle)],
+        [np.sin(angle), np.cos(angle)]
+    ])
+    
+    # Initialize segments list
+    segments = []
+
+    # Scale, rotate and translate each segment
+    for segment in norm_segments:
+        new_segment = np.zeros_like(segment)
+        for idx, point in enumerate(segment):
+            # Scale
+            scaled_point = point * size
+            # Rotate
+            rotated_point = np.dot(rot_matrix, scaled_point)
+            # Translate
+            translated_point = rotated_point + bottom
+            # Store new point
+            new_segment[idx] = translated_point
+        segments.append(new_segment)
+        
+    return segments
 
 # Define the drawing speed in units per second
 draw_speed = 4.0
@@ -29,34 +58,18 @@ segment_delay = 0.1
 
 fig, ax = plt.subplots()
 
-def transform_segment(base, transform):
-    scale, rotate, translate = transform
-    # Apply the scale and rotation
-    base = np.dot(base, np.array([[np.cos(rotate), -np.sin(rotate)], [np.sin(rotate), np.cos(rotate)]])) * scale
-    # Apply the translation
-    base += translate
-    return base
-
-def fractal_pattern(base, generations):
-    if generations == 0:
-        return base
-    else:
-        output = []
-        for segment in base:
-            # Define the transformations for this segment
-            scale = np.sqrt(np.sum((segment[-1] - segment[0])**2))
-            rotate = np.arctan2(segment[-1][1] - segment[0][1], segment[-1][0] - segment[0][0])
-            translate = segment[0]
-            # Apply the transformations to the base shape
-            transformed = [transform_segment(seg, (scale, rotate, translate)) for seg in base]
-            # Add the transformed shape to the output
-            output.extend(transformed)
-        # Recurse for the next generation
-        return fractal_pattern(output, generations - 1)
-
-
 # set the figure background to black
 fig.patch.set_facecolor('black')
+
+# Define the top and bottom points of the 'stussy s'
+top = np.array([1, 1])
+bottom = np.array([-1, -1])
+
+# Create the segments for the 'stussy s'
+segments = create_stussy_s_segments(top, bottom)
+
+# Calculate total length of all segments (including delays)
+total_time = sum(np.sqrt(np.sum(np.diff(segment, axis=0)**2, axis=1)).sum() / draw_speed + segment_delay for segment in segments)
 
 # Creating empty line objects
 lines = [ax.plot([], [], lw=2, color='white')[0] for _ in segments]
@@ -71,28 +84,24 @@ def init():
         line.set_data([], [])
     return lines
 
-# Define the base shape and number of generations
-base = segments
-generations = 3
-
-# Generate the fractal pattern
-segments = fractal_pattern(base, generations)
-
-# Calculate total length of all segments
-total_time = len(segments)
-
 # Animation function
 def animate(i):
-    # Clear the current line data
-    for line in lines:
-        line.set_data([], [])
-    # Draw the current segment
-    if i < len(segments):
-        lines[i % len(lines)].set_data(segments[i][:, 0], segments[i][:, 1])
+    time = i * (1.0/60.0)  # convert frame number to time
+    total_time = 0
+    for j, segment in enumerate(segments):
+        segment_len = np.sqrt(np.sum((segment[-1] - segment[0])**2))
+        segment_time = segment_len / draw_speed
+        if total_time <= time < total_time + segment_time:
+            t = (time - total_time) / segment_time
+            point = segment[0] + t * (segment[-1] - segment[0])
+            lines[j].set_data(*zip(segment[0], point))
+            return lines
+        elif total_time <= time < total_time + segment_time + segment_delay:
+            lines[j].set_data(segment[:, 0], segment[:, 1])
+            return lines
+        lines[j].set_data(segment[:, 0], segment[:, 1])
+        total_time += segment_time + segment_delay
     return lines
-
-# Call the animator
-ani = animation.FuncAnimation(fig, animate, init_func=init, frames=total_time, interval=1000.0/frame_rate, blit=True, repeat=False)
 
 # Set the frame rate (frames per second)
 frame_rate = 60
